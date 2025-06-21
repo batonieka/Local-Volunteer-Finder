0aae24276feaa5364b6a9a6b1164bc5930e1c40e
kiu_khelidze.ekaterine <khelidze.ekaterine@kiu.edu.ge>
2025-06-21 18:39:46 +0400

    adding footer

---
+++ frontend/src/components/Footer.tsx
@@ -0,0 +1,9 @@
+export const Footer = () => (
+  <footer className="bg-gray-100 border-t text-center text-sm py-4 mt-10">
+    © 2024 VolunteerHub. All rights reserved.
+    <div className="mt-2 space-x-4">
+      <a href="#">About Us</a>
+      <a href="#">Contact</a>
+    </div>
+  </footer>
+);
