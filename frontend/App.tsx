29929be8f8952457d0e08ebea5fff91db730ad01
kiu_khelidze.ekaterine <khelidze.ekaterine@kiu.edu.ge>
2025-06-21 07:04:29 +0400

    adding not found page

--- frontend/App.tsx
+++ frontend/App.tsx
@@ -1,12 +1,14 @@
 import { Routes, Route } from "react-router-dom";
 import { HomePage } from "./pages/HomePage";
 import { DetailsPage } from "./pages/DetailsPage";
+import { NotFoundPage } from "./pages/NotFoundPage";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<HomePage />} />
       <Route path="/opportunity/:id" element={<DetailsPage />} />
+      <Route path="*" element={<NotFoundPage />} /> {/* ✅ 404 route */}
     </Routes>
   );
 }
